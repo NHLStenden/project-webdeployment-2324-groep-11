a1f73dabd6a25d2c3d0ed8146ab03f74528df9ef
Yucel <yucel.demir@student.nhlstenden.nl>
2024-05-21 11:08:39 +0200

    Originele Brandstof cafe verwijderd
    Ik heb de originele Blazor file verwijderd. Hij staat nog op de laptop van Yucel als er eventueel nog dingen van nodig zijn

--- BrandstofCafe/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace BrandstofCafe.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
