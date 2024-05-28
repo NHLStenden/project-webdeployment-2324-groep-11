eb169efefcaf2df7cb4e4ee3fba738ce8d809ba3
Yucel <yucel.demir@student.nhlstenden.nl>
2024-05-28 19:01:18 +0200

    Home menu Update 2
    Nogmaals een update voor de home pagina. De footer is toegevoegd en de kaarten zijn aangepast

--- DieselBrandstofCafe/DieselBrandstofCafe/obj/Debug/net8.0/DieselBrandstofCafe.AssemblyInfo.cs
+++ DieselBrandstofCafe/DieselBrandstofCafe/obj/Debug/net8.0/DieselBrandstofCafe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c1b59dd28c07cb860095ff368a93c7aeda0b23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f58314ea7b77e956ad139b5bb4965959618f10a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
