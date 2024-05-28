07c0ee5f0f1e6eaf6ed239ef25613f79b01936e5
MartijnHuisma <martijnhuisma@gmail.com>
2024-05-28 12:21:53 +0200

    Menu pagina
    Aanpassingen gemaakt.

--- DieselBrandstofCafe/DieselBrandstofCafe/obj/Debug/net8.0/DieselBrandstofCafe.AssemblyInfo.cs
+++ DieselBrandstofCafe/DieselBrandstofCafe/obj/Debug/net8.0/DieselBrandstofCafe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33445d624047880ddb552d0b8b6b0777d2c656cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acd110e58ee001880cb7480d6c1d1f150a72576d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
