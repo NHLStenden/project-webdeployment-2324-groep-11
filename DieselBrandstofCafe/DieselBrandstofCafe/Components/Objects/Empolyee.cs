3c23354c5e6dba908ca0f7d1fa72f2b8224b1dee
Yazan <yaintercode@gmail.com>
2024-05-30 23:02:34 +0200

    Added a new Objects directory, added two new objects and fixed some stuff

---
+++ DieselBrandstofCafe/DieselBrandstofCafe/Components/Objects/Empolyee.cs
@@ -0,0 +1,19 @@
+﻿namespace DieselBrandstofCafe.Components.Objects
+{
+    public class Empolyee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public int EmployeeAge { get; set; } = 0;
+        public string EmployeeEmail { get; set; }
+        public string EmployeePhone { get; set; }
+        public int? SupervisorID { get; set; }  // Foreign key to the supervisor
+
+
+        // Navigation property for the supervisor
+        public Empolyee Supervisor { get; set; }
+
+        // Navigation property for the employees supervised by this employee
+        public List<Empolyee> Subordinates { get; set; }
+    }
+}
