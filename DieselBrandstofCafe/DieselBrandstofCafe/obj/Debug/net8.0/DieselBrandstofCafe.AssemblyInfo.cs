f298a75dce7777ee3fbefbb74dae859f23c4331b
MartijnHuisma <martijnhuisma@gmail.com>
2024-05-30 22:19:37 +0200

    .

--- DieselBrandstofCafe/DieselBrandstofCafe/obj/Debug/net8.0/DieselBrandstofCafe.AssemblyInfo.cs
+++ DieselBrandstofCafe/DieselBrandstofCafe/obj/Debug/net8.0/DieselBrandstofCafe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+062138444ae6045e7b9b7691c577f05aeba79a45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+343e2d3186949af867d9cc0bc72d6f2bd9cfc964")]
 [assembly: System.Reflection.AssemblyProductAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
