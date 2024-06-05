8a8f2e6200612db1e7aa3a6867194e629f5f1dd0
BrentKoers2004 <brent.koers@student.nhlstenden.com>
2024-06-05 12:55:49 +0200

    BetalingService

--- DieselBrandstofCafe/DieselBrandstofCafe/obj/Debug/net8.0/DieselBrandstofCafe.AssemblyInfo.cs
+++ DieselBrandstofCafe/DieselBrandstofCafe/obj/Debug/net8.0/DieselBrandstofCafe.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c146f66123196da3a7ada45e2a20807c2575d0ac")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+811403752c7368293aa2bb6fcd63165905e482d0")]
->>>>>>> Stashed changes
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4244293e93570fb8db56ddfbf97a51d93de1d08c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
