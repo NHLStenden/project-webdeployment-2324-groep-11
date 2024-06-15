678f0aac6f5bbacc7b12b5854be2f3a6630bc51e
Yucel <yucel.demir@student.nhlstenden.nl>
2024-06-15 20:20:34 +0200

    Complete visual overhaul
    -The following pages got a visual update that fit with the rest of the website: Menu, My Orders, Employee Orders
    
    -you are now able to see In Progress in The order sections

--- DieselBrandstofCafe/DieselBrandstofCafe/obj/Debug/net8.0/DieselBrandstofCafe.AssemblyInfo.cs
+++ DieselBrandstofCafe/DieselBrandstofCafe/obj/Debug/net8.0/DieselBrandstofCafe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daab98ff4b1a092e4ec6e0a1e070fe1a98f50491")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b629a6cab645dc3a53349fb1ecae89e065254f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
