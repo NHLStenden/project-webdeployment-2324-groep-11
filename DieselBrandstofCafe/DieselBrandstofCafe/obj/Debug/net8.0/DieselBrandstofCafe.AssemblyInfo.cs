5c174d8393abb8dcba70ca33490d03583f1f332f
Yucel <yucel.demir@student.nhlstenden.nl>
2024-06-18 00:13:04 +0200

    update

--- DieselBrandstofCafe/DieselBrandstofCafe/obj/Debug/net8.0/DieselBrandstofCafe.AssemblyInfo.cs
+++ DieselBrandstofCafe/DieselBrandstofCafe/obj/Debug/net8.0/DieselBrandstofCafe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+295b71799e6bff9b7ab3c0b4182ae9b07500f072")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e2a4020aa31281f0fb35e1ffb1a20ebb2b0d22")]
 [assembly: System.Reflection.AssemblyProductAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
