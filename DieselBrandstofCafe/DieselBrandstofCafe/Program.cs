916af50ddcd993de3f680d48e0b07895ba754bcf
X0rbit3 <yaintercode@gmail.com>
2024-06-18 10:05:05 +0200

    - Added a new Dockerfile - Added a new docker-compose file - Moved the Dockerfile and docker-compose files to the root directory - Implemented a special shell script that checks for the MySQL server for it to be able to run the webapp server - Made some changes to the .gitignore file - Fixed some minor issues

--- DieselBrandstofCafe/DieselBrandstofCafe/Program.cs
+++ DieselBrandstofCafe/DieselBrandstofCafe/Program.cs
@@ -49,8 +49,12 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    // Disable HTTPS redirection for development
+    app.UseHttpsRedirection();
 }
 
 app.UseHttpsRedirection();
