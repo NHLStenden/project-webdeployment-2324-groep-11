0b0f2bf9ac79162aee65ca024f07b9e3f335809c
MartijnHuisma <martijnhuisma@gmail.com>
2024-06-21 16:02:14 +0200

    Removed data

--- DieselBrandstofCafe/DieselBrandstofCafe/obj/Debug/net8.0/DieselBrandstofCafe.AssemblyInfo.cs
+++ DieselBrandstofCafe/DieselBrandstofCafe/obj/Debug/net8.0/DieselBrandstofCafe.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+916af50ddcd993de3f680d48e0b07895ba754bcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4899c7f6953862092cf4d12abf80b132569f454")]
 [assembly: System.Reflection.AssemblyProductAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DieselBrandstofCafe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
